d1216900e48ef7a856296f6de776e205043d2e81
Salman Saputra <salmansaputra76@gmail.com>
2026-03-02 01:57:23 +0700

    Update dashboard page

---
+++ app/Filament/Pages/Dashboard/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Pages\Dashboard;
+
+use Filament\Pages\Page;
+
+class Dashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected static ?string $title = 'Dashboard';
+
+    protected static string $view = 'filament.pages.dashboard';
+}
---
+++ resources/views/filament/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+<div class="filament-page">
+    <div class="py-12">
+        <div class="w-full sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="p-6 text-gray-900">
+                    You're logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
